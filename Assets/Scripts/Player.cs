a04c532b44520b807c7115978bdeb53d60fd0948
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-07-26 14:56:37 +0900

    7/26

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    Vector2 inputVec;
+    public Vector2 inputVec;
 
-    void Start()
+    public float speed;
+
+    Rigidbody2D rigid;
+
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime; // 속도 제어
+        rigid.MovePosition(rigid.position + nextVec);
     }
 }
