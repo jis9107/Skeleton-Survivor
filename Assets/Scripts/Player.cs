afe4dd30c89be753b0e33e1c98baacf67b2462e9
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-07-26 14:39:04 +0900

    업로드

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Vector2 inputVec;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
