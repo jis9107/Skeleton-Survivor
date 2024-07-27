a030e3eeafe4795d0fcd26a5d1fcf5cebf08673e
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-07-27 18:43:03 +0900

    7/27

--- Assets/Scripts/Reposition.cs
+++ Assets/Scripts/Reposition.cs
@@ -13,6 +13,29 @@ public class Reposition : MonoBehaviour
         Vector3 myPos = transform.position;
 
         float diffX = Mathf.Abs(playerPos.x - myPos.x);
-        float diffy = Mathf.Abs(playerPos.y - myPos.y);
+        float diffY = Mathf.Abs(playerPos.y - myPos.y);
+
+        Vector3 playerDir = GameManager.instance.player.inputVec;
+        float dirX = playerDir.x < 0 ? -1 : 1;
+        float dirY = playerDir.y < 0 ? -1 : 1;
+
+        switch(transform.tag)
+        {
+            case "Ground":
+                if(diffX > diffY)
+                {
+                    transform.Translate(Vector3.right * dirX * 40);
+                }
+                else
+                {
+                    transform.Translate(Vector3.up * dirY * 40);
+                }
+
+                break;
+
+            case "Enemy":
+
+                break;
+        }
     }
 }
