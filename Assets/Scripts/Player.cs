4066f4ba065dc9bd6d2dd8d7a1c630ab1e251fe3
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-07-27 17:29:21 +0900

    7/27

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,11 +10,22 @@ public class Player : MonoBehaviour
     public float speed;
 
     Rigidbody2D rigid;
+    SpriteRenderer spriter;
+    Animator anim;
 
     void Awake()
     {
         rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>();
+        anim = GetComponent<Animator>();
     }
+
+    private void Update()
+    {
+        inputVec.x = Input.GetAxisRaw("Horizontal");
+        inputVec.y = Input.GetAxisRaw("Vertical");
+    }
+
 
     private void FixedUpdate()
     {
@@ -22,8 +33,18 @@ public class Player : MonoBehaviour
         rigid.MovePosition(rigid.position + nextVec);
     }
 
-    void OnMove(InputValue value)
+    //void OnMove(InputValue value)
+    //{
+    //    inputVec = value.Get<Vector2>(); //Input System에 noramlized를 사용하고 있어 nextVec에 normalized 생략가능 
+    //}
+
+    private void LateUpdate() // 업데이트가 끝나고 다음 프레임으로 넘어갈 때 실행된다.
     {
-        inputVec = value.Get<Vector2>(); //Input System에 noramlized를 사용하고 있어 nextVec에 normalized 생략가능 
+        anim.SetFloat("Speed", inputVec.magnitude);
+
+        if(inputVec.x != 0)
+        {
+            spriter.flipX = inputVec.x < 0;
+        }
     }
 }
