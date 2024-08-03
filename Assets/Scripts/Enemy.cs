2dc1e0f5dc67ad5b29844eb58ad4782c5e8f3d32
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-08-03 14:22:28 +0900

    08/03

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public Rigidbody2D target;
+
+    public float speed;
+
+    bool isLive;
+
+
+    Rigidbody2D rigid;
+    SpriteRenderer spriter;
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        spriter = GetComponent<SpriteRenderer>(); 
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        Vector2 dirVec = target.position - rigid.position;
+        Vector2 nextVec = dirVec.normalized * speed * Time.fixedDeltaTime;
+        rigid.MovePosition(rigid.position + nextVec);
+        rigid.velocity = Vector2.zero;
     }
 }
