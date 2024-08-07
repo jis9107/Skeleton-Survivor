ccdafb60ea9e271b982f9cddf1e9c2e3bf852a70
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-08-07 19:08:20 +0900

    08/07

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    public int id;
+    public int prefabId;
+    public float damage;
+    public int count;
+    public float speed;
+
+
+    void Update()
+    {
+        
+    }
+
+    public void Init()
+    {
+        switch (id)
+        {
+            case 0:
+
+                break;
+        }
+    }
+}
