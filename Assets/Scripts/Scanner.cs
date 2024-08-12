160aed4ce5829828e0eebb54b1bb73d55d23d967
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-08-12 18:22:58 +0900

    Update Scanner.cs

--- Assets/Scripts/Scanner.cs
+++ Assets/Scripts/Scanner.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Scanner : MonoBehaviour
 {
     public float scanRange;
+
     public LayerMask targetLayer;
     public RaycastHit2D[] targets;
+    public Transform nearestTarget;
+
+    private void FixedUpdate()
+    {
+        targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0);
+    }
 }
+
