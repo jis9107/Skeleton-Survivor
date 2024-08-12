10793236ddfe96274cba5da90d58f6e7894f96a0
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-08-12 18:25:54 +0900

    Update Scanner.cs

--- Assets/Scripts/Scanner.cs
+++ Assets/Scripts/Scanner.cs
@@ -12,7 +12,23 @@ public class Scanner : MonoBehaviour
 
     private void FixedUpdate()
     {
-        targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0);
+        targets = Physics2D.CircleCastAll(transform.position, scanRange, Vector2.zero, 0, targetLayer);
+                                        //(캐스팅 시작 위치, 원의 반지름, 캐스팅 방향, 캐스팅 길이, 대상 레이어)
+    }
+
+    Transform GetNearest()
+    {
+        Transform result = null;
+        float diff = 100;
+
+        foreach (RaycastHit2D target in targets)
+        {
+            Vector3 myPos = transform.position;
+            Vector3 targetPos = target.transform.position;
+
+        }
+
+        return result;
     }
 }
 
