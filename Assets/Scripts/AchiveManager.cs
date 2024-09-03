52a36ed575ae10e32dba1e62220a7f10af042e39
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-09-03 23:43:49 +0900

    Update AchiveManager.cs

--- Assets/Scripts/AchiveManager.cs
+++ Assets/Scripts/AchiveManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,19 @@ public class AchiveManager : MonoBehaviour
 {
     public GameObject[] lockCharacter;
     public GameObject[] unLockCharacter;
+
+    enum Achive //업적들
+    {
+        UnLockPotato,
+        UnLickBean
+    }
+
+    Achive[] achives; // 저장소
+
+    private void Awake()
+    {
+        achives = (Achive[])Enum.GetValues(typeof(Achive)); // enum 타입 값을 가져온다.
+    }
 
     private void Start()
     {
