fe91bcb1f446bcbc86c0262904ea1eb3d63d2084
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-09-16 16:01:33 +0900

    인게임 아이템 제작

--- Assets/Scripts/Exp.cs
+++ Assets/Scripts/Exp.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class Exp : MonoBehaviour
 {
+
+    public Rigidbody2D target;
+
+    private void OnEnable()
+    {
+        target = GameManager.instance.player.GetComponent<Rigidbody2D>();
+    }
+
     private void OnTriggerEnter2D(Collider2D col)
     {
         if (col.gameObject.name == "Player")
--- Assets/Scripts/InGameItem.cs
+++ Assets/Scripts/InGameItem.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem.Switch;
 using static ItemData;
 
 public class InGameItem : MonoBehaviour
@@ -13,5 +14,37 @@ public class InGameItem : MonoBehaviour
     }
 
     public Type itemTpye;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+            return;
+
+        switch(itemTpye)
+        {
+            case Type.bomb:
+                StartCoroutine("Bomb");
+                this.gameObject.SetActive(false);
+                break;
+
+            case Type.coin:
+                GameManager.instance.GetInGameMoney(1000);
+                this.gameObject.SetActive(false);
+                break;
+
+            case Type.mag:
+                this.gameObject.SetActive(false);
+                break;
+        }
+    }
+
+    IEnumerator Bomb()
+    {
+        GameManager.instance.enemyCleaner.SetActive(true);
+
+        yield return new WaitForSeconds(0.1f);
+
+        GameManager.instance.enemyCleaner.SetActive(false);
+    }
 
 }
