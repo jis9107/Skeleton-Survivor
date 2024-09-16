d9caf38a4e875e4444a6fd8ce85f17e28a3eebc3
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-09-16 16:15:30 +0900

    인게임 아이템 제작

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
-    public GameObject[] ChestItem;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Bullet"))
             return;
 
-        int random = Random.Range(0, ChestItem.Length);
+        int random = Random.Range(0, 5);
 
         if (random <= 2)
             random = 2;
