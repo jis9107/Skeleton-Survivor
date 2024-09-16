5c1477de0a747b422b3431211676cd97ad5140d3
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-09-16 15:42:23 +0900

    인게임 아이템 제작

---
+++ Assets/Scripts/InGameItem.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameItem : MonoBehaviour
+{
+   public enum Type
+    {
+        bomb,
+        coin,
+        mag
+    }
+   
+}
