5e76ab88936bd24dbba2d7a00eea3c87e40cd38c
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-09-16 15:26:18 +0900

    Update Chest.cs

--- Assets/Scripts/Chest.cs
+++ Assets/Scripts/Chest.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Chest : MonoBehaviour
 {
+    public GameObject[] ChestItem;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (!collision.CompareTag("Bullet"))
