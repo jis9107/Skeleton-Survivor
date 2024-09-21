91628d698ca0f2b22a54f1d1969d38e17d71097a
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-09-21 13:18:43 +0900

    캐릭터 선택

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -1,18 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public CharacterData[] charData;
+
+    public Image icon;
+    public Text charName;
+    public Text charLv;
+    public Text charDamage;
+    public Text charHealth;
+
+    // 추후에 저장 할 데이터
+    public int nowCharacterId;
+
+    private void Awake()
     {
-        
+        Init(nowCharacterId);
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnSelectCharacter(int id)
     {
-        
+        nowCharacterId = id;
+        icon.sprite = charData[id].charImage;
+        charName.text = charData[id].charName;
+        charDamage.text = charData[id].damage.ToString();
+        charHealth.text = charData[id].maxHealth.ToString();
+    }
+
+    public void OnClickSelectButton()
+    {
+        GameManager.instance.playerId = charData[nowCharacterId].charId;
+        GameManager.instance.maxHealth = charData[nowCharacterId].maxHealth;
+    }
+
+    public void Init(int id)
+    {
+        nowCharacterId = id;
+        icon.sprite = charData[id].charImage;
+        charName.text = charData[id].charName;
+        charDamage.text = charData[id].damage.ToString();
+        charHealth.text = charData[id].maxHealth.ToString();
+        GameManager.instance.playerId = charData[nowCharacterId].charId;
+        GameManager.instance.maxHealth = charData[nowCharacterId].maxHealth;
     }
 }
