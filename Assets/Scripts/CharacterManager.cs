589482b1dbe6e5004c7c0d530bbfea88debb2b24
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-09-25 09:48:37 +0900

    캐릭터 데이터 저장 및 불러오기

--- Assets/Scripts/CharacterManager.cs
+++ Assets/Scripts/CharacterManager.cs
@@ -23,13 +23,8 @@ public class CharacterManager : MonoBehaviour
     {
         //데이터 매니저에서 받아올 값 (레벨, 현재 캐릭터 Id)
         dataManager = GetComponent<DataManager>();
-/*        for(int i = 0; i < charData.Length; i++)
-        {
-            charData[i].level = dataManager.charlevel[i];
-        }*/
-        
-        nowCharacterId = 0; // 나중에 받아올 값
-        Init(nowCharacterId);
+        Init();
+        OnSelectCharacter(nowCharacterId);
     }
 
     public void OnSelectCharacter(int id)
@@ -40,23 +35,27 @@ public class CharacterManager : MonoBehaviour
         charLv.text = charData[id].level.ToString();
         charDamage.text = charData[id].damage.ToString();
         charHealth.text = charData[id].maxHealth.ToString();
+
+        CharDataSave();
     }
 
-    public void OnClickSelectButton()
+    public void OnClickSelectButton() // 캐릭터 선택 버튼을 눌렀을 시 GamaManager에 변수를 선택 된 캐릭터의 데이터로 변환하는 함수
     {
         GameManager.instance.charDamage = charData[nowCharacterId].damage;
         GameManager.instance.playerId = charData[nowCharacterId].charId;
         GameManager.instance.maxHealth = charData[nowCharacterId].maxHealth;
     }
 
-    public void Init(int id)
+    public void Init()
     {
-        nowCharacterId = id;
-        icon.sprite = charData[id].charImage;
-        charName.text = charData[id].charName;
-        charLv.text = charData[id].level.ToString();
-        charDamage.text = charData[id].damage.ToString();
-        charHealth.text = charData[id].maxHealth.ToString();
+        nowCharacterId = PlayerPrefs.GetInt("selectCharId");
+
+        for(int i = 0; i < charData.Length; i++)
+        {
+            charData[i].level = PlayerPrefs.GetInt($"characterLevel {i}");
+            charData[i].damage = charData[i].level * 10;
+            charData[i].maxHealth = charData[i].level * 15;
+        }
     }
 
     public void CharacterUpgrade()
@@ -64,6 +63,20 @@ public class CharacterManager : MonoBehaviour
         charData[nowCharacterId].level += 1;
         charData[nowCharacterId].damage = 10 * (charData[nowCharacterId].level);
         charData[nowCharacterId].maxHealth = 10 * (charData[nowCharacterId].level);
-        Init(nowCharacterId);
+        CharDataSave();
+        Init();
+    }
+
+    public void CharDataSave()
+    {
+        for(int i = 0; i < charData.Length;i++)
+        {
+            PlayerPrefs.SetInt($"characterID {i}", charData[i].charId); // 캐릭터 아이디 저장
+            PlayerPrefs.SetInt($"characterLevel {i}", charData[i].level); // 캐릭터 레벨 저장
+        }
+
+        PlayerPrefs.SetInt("selectCharId", nowCharacterId);
+
+        PlayerPrefs.Save();
     }
 }
