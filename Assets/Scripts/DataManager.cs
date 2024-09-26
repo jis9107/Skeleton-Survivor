f571ace0a78b117d817ac5f7b5c710816ba9ff76
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-09-26 16:24:45 +0900

    업적 데이터

--- Assets/Scripts/DataManager.cs
+++ Assets/Scripts/DataManager.cs
@@ -1,13 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DataManager : MonoBehaviour
 {
+    [Header("#AchiveText")]
+    public Text totalKillText;
+    public Text totalmoneyText;
+    public Text totalPlayTimeText;
+
+    [Header("# GameData")]
+    public int curMoney;
+    public int luby;
+
+    int totalKill;
+    int totalMoney;
+    int totalPlayTime;
+
+
     public void Save()
     {
         // 저장 할 변수들
-        // 1. 캐릭터 레벨
         // 2. 스테이지
         // 3. 업적
         // 4. 돈, 보석
