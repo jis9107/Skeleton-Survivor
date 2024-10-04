703b3ddec07f92ca8eae0f2787205d7d456612cf
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-10-04 09:35:29 +0900

    닉네임 신 제작

---
+++ Assets/Scripts/LogIn.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LogIn : MonoBehaviour
+{
+    public InputField inputNick;
+
+    public Button setNickName;
+
+
+    private void Awake()
+    {
+        if (PlayerPrefs.HasKey("PlayerNickName"))
+            SceneManager.LoadScene(1);
+    }
+    void OnClickSetButton()
+    {
+        string nick = inputNick.text;
+        PlayerPrefs.SetString("PlayerNickName", nick);
+        SceneManager.LoadScene(1);
+    }
+}
