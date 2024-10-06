2be84455417ea25785463fb94c331368df2a5ab4
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-10-06 09:50:07 +0900

    Update AdmobBanner.cs

--- Assets/_My/AdmobBanner.cs
+++ Assets/_My/AdmobBanner.cs
@@ -1,9 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GoogleMobileAds.Api;
 
 public class AdmobBanner : MonoBehaviour
 {
+    private readonly string unitId = "";
+    private readonly string test_unitId = ""
+    
     // Start is called before the first frame update
     void Start()
     {
