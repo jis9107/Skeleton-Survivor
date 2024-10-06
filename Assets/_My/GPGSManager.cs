9e388e1663dfb58f44b4be026ebd93416326fc80
jis9107 <117075698+jis9107@users.noreply.github.com>
2024-10-06 09:10:10 +0900

    GPGS

--- Assets/_My/GPGSManager.cs
+++ Assets/_My/GPGSManager.cs
@@ -1,6 +1,8 @@
 using GooglePlayGames;
 using UnityEngine;
 using UnityEngine.UI;
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
 
 public class GPGSManager : MonoBehaviour
 {
@@ -14,5 +16,19 @@ public class GPGSManager : MonoBehaviour
     public void GPGS_LogIn()
     {
         PlayGamesPlatform.Instance.Authenticate(ProcessAuthentication);
+    }
+
+    internal void ProcessAuthentication(SignInStatus status) // 11버전은 로그아웃이 X
+    {
+        if (status == SignInStatus.Success)
+        {
+            // Continue with Play Games Services
+        }
+        else
+        {
+            // Disable your integration with Play Games Services or show a login button
+            // to ask users to sign-in. Clicking it should call
+            // PlayGamesPlatform.Instance.ManuallyAuthenticate(ProcessAuthentication).
+        }
     }
 }
